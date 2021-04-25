052dc6ca6893bb1d06a953b69ec544fa1343850f
Neat_Username <67394007+NeatUsername@users.noreply.github.com>
2021-04-25 08:49:32 -0400

    troubleshooting data layer

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -14,35 +14,37 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   accessToken: API_KEY
 }).addTo(myMap);
 
-// // Store API query variables
-// var baseURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
+// Store API query variables
+var baseURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
 
-// // Assemble API query URL
-// var url = baseURL;
+// Assemble API query URL
+var url = baseURL;
 
-// // Grab the data with d3
-// d3.json(url).then(function(response) {
+// Grab the data with d3
+d3.json(url).then(function(response) {
+  // console.log(response)
+  // Create a new marker cluster group
+  var markers = L.markerClusterGroup();
 
-//   // Create a new marker cluster group
-//   var markers = L.markerClusterGroup();
+  // Loop through data
+  for (var i = 0; i < response.length; i++) {
 
-//   // Loop through data
-//   for (var i = 0; i < response.length; i++) {
+    // Set the data location property to a variable
+    var location = response[i].features.geometry.coordinates;
+    console.log(location)
 
-//     // Set the data location property to a variable
-//     var location = response[i].location;
+  //   // Check for location property
+  //   if (location) {
 
-//     // Check for location property
-//     if (location) {
+  //     // Add a new marker to the cluster group and bind a pop-up
+  //     markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
+  //       .bindPopup(response[i].features.properties.mag));
+  //   }
 
-//       // Add a new marker to the cluster group and bind a pop-up
-//       markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
-//         .bindPopup(response[i].descriptor));
-//     }
+  // }
 
-//   }
+  // // Add our marker cluster layer to the map
+  // myMap.addLayer(markers);
 
-//   // Add our marker cluster layer to the map
-//   myMap.addLayer(markers);
-
-// });
+    }
+});
